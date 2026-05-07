e9210b0af669ad34aebef3ce626a79bac700b9ed
Mallika <mallikalife@gmail.com>
2026-05-07 16:04:34 +0530

    url change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/' || 'https://resplendent-upliftment-production-beed.up.railway.app'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/' || 'https://team-task-manager-production-8292.up.railway.app/api/login/app'
 });
 
 api.interceptors.request.use((config) => {
