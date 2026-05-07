92ee1c38f448f05bf03c9f3d43f5a1ee86446e3b
Mallika <mallikalife@gmail.com>
2026-05-07 13:40:29 +0530

    updated backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
 from api.views import SignupView, ProjectViewSet, TaskViewSet, MemberViewSet, UserMeView
+
+def api_root(request):
+    return JsonResponse({"message": "Team Task Manager API is running successfully!"})
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
@@ -10,6 +14,7 @@ router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'members', MemberViewSet, basename='member')
 
 urlpatterns = [
+    path('', api_root, name='root'),
     path('admin/', admin.site.urls),
     path('api/signup/', SignupView.as_view(), name='signup'),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
